fe8d82bf3a621b2c0f9aae8e4ea1391871b8a5f6
Rich Lander <rich@lander.ca>
2024-03-12 09:23:36 -0700

    Add notes for solver

---
+++ Solver/Solvers/NakedTriplesSolver.cs
@@ -0,0 +1,117 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Sudoku;
+
+public class NakedTriplesSolver : ISolver
+{
+        public string Name => nameof(NakedTriplesSolver);
+
+    /*
+        Naked triples: matching tiples are three locked cells that contain (in aggregate) the same three candidates and allow removal of those candidates from other cells in the same unit
+        Can be in 3/3/3, 3/3/2, 3/2/2, or 2/2/2 forms (number of candidates in each of the three cells).
+
+        2,0 (18); Aligned: 1, 2; Aligned Candidates: 1, 6; Removals: 1
+        0,3 ( 3); Aligned: 1, 2; Aligned Candidates: 1, 6; Removals: 1
+        0,4 ( 4); Aligned: 1, 2; Aligned Candidates: 1, 6; Removals: 1, 6
+        0,5 ( 5); Aligned: 1, 2; Aligned Candidates: 1, 6; Removals: 6
+          * *
+        4 0 0 | 0 0 0 | 9 3 8*
+        0 3 2 | 0 9 4 | 1 0 0
+        0 9 5 | 3 0 0 | 2 4 0
+        ------+-------+------
+        3 7 0 | 6 0 9 | 0 0 4
+        5 2 9 | 0 0 1 | 6 7 3
+        6 0 4 | 7 0 3 | 0 9 0
+        ------+-------+------
+        9 5 7 | 0 0 8 | 3 0 0
+        0 0 3 | 9 0 0 | 4 0 0
+        2 4 0 | 0 3 0 | 7 0 9
+
+        https://www.sudokuwiki.org/sudoku.htm?bd=400000938032094100095300240370609004529001673604703090957008300003900400240030709
+
+        Task: Find a cell with two candidates where another cell in same unit has the same two candidates.
+        Remove those values (either one or both) from other cells in the unit.
+    */
+    public bool TrySolve(Puzzle puzzle, Cell cell, [NotNullWhen(true)] out Solution? solution)
+    {
+        solution = null;
+        IReadOnlyList<int> cellCandidates = puzzle.GetCellCandidates(cell);
+
+        if (cellCandidates.Count is not 2)
+        {
+            return false;
+        }
+
+        List<IEnumerable<int>> lines = [
+            Puzzle.GetBoxIndices(cell.Box),
+            Puzzle.GetRowIndices(cell.Row),
+            Puzzle.GetColumnIndices(cell.Column)];
+
+        foreach (IEnumerable<int> line in lines)
+        {
+            // Which index has a matching pair to cell?
+            if (TryFindCandidatePairsMatchCell(puzzle, cell, line, out int uniqueIndex))
+            {
+                if (uniqueIndex < cell)
+                {
+                    continue;
+                }
+
+                // Remove values from other cells in line
+                foreach (int index in line.Where(x => !(puzzle.IsCellSolved(x) || x == cell || x == uniqueIndex)))
+                {
+                    IReadOnlyList<int> neighborCandidates = puzzle.GetCellCandidates(index);
+
+                    if (neighborCandidates.Intersect(cellCandidates).Any())
+                    {
+                        // List<int> removals = neighborCandidates.Except(cellCandidates).ToList();
+                        Solution s = new(puzzle.GetCell(index), -1, Name)
+                        {
+                            RemovalCandidates = neighborCandidates.Intersect(cellCandidates).ToList(),
+                            AlignedCandidates = cellCandidates,
+                            AlignedIndices = [cell, uniqueIndex],
+                        };
+                        solution = Puzzle.UpdateSolutionWithNextSolution(solution, s);
+                    }
+                }
+            }
+        }
+
+        if (solution is null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // Finds unique index in line that contains a candidate pair (and only that pair)
+    public static bool TryFindCandidatePairsMatchCell(Puzzle puzzle, Cell cell, IEnumerable<int> line, out int matchIndex)
+    {
+        Dictionary<int, List<int>> matches = [];
+        IReadOnlyList<int> cellCandidates = puzzle.GetCellCandidates(cell);
+        foreach (int neighborIndex in line.Where(x => x !=cell))
+        {
+            IReadOnlyList<int> neighborCandidates = puzzle.GetCellCandidates(neighborIndex);
+            foreach (int match in neighborCandidates.Intersect(cellCandidates))
+            {
+                matches 
+                return true;
+            }
+        }
+
+        matchIndex = -1;
+        return false;
+    }
+}
+
+
+/*
+    Algorithm:
+    - Candidate shows up only in box row or column
+    - Candidate is in <= 3 cells (that's an implication of the point above)
+    - Candidate is in two cells, both with two candidates, and they matches (then naked pair)
+    - Candidate is in three cells, all with three candidates, and they match (then naked triple)
+    - Candidate is in three cells, all with two or three candidates, all with just three candidates in total (then naked triple)
+    - Candidate is in two cells, all with two candidates, all with just three candidates in total (then naked triple)
+*/
