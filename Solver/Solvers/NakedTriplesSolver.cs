a42166eafad2a03ca51eb0fb5f8f5b1a446ea3b1
Richard Lander <rlander@microsoft.com>
2024-04-14 14:51:17 -0700

    Remove dead code

--- Solver/Solvers/NakedTriplesSolver.cs
+++ Solver/Solvers/NakedTriplesSolver.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.IO.Compression;
 
 namespace Sudoku;
 
@@ -43,15 +42,6 @@ public class NakedTriplesSolver : ISolver
         - That's either a pair or a triple, dependending on the details.
         - Otherwise ... not a pair, and maybe a triple
         - Continue the algorithm for 3/3/2 and 2/2/2 triple forms
-        
-        Approach for a cell with 2 candidates:
-        - This is the most interesting scenario
-        - Do the same thing, which is to get all the partial matches
-        - This will naturally get all the cells which would be involved in 3/3/2, 3/2/2, and 2/2/2 form
-        - For each partial match, do a union on all the other matches (one at a time) + the cell
-        - If this results in 3 candidates, then we have a naked triple
-        - We can determine the form at that point
-
     */
     public bool TrySolve(Puzzle puzzle, Cell cell, [NotNullWhen(true)] out Solution? solution)
     {
@@ -219,77 +209,6 @@ public class NakedTriplesSolver : ISolver
         matches = new();
         return false;
     }
-
-    // private static List<int> FindMatches(Puzzle puzzle, IReadOnlyList<int> cellCandidates, List<int> partialMatches)
-    // {
-    //     int candidateCount = cellCandidates.Count;
-    //     List<int> matches = [];
-
-    //     foreach (int index in partialMatches)
-    //     {
-    //         IReadOnlyList<int> candidates = puzzle.GetCellCandidates(index);
-    //         int intersectionCount = cellCandidates.Intersect(candidates).Count();
-    //         if (candidateCount == intersectionCount)
-    //         {
-    //             matches.Add(index);
-    //         }
-    //     }
-
-    //     return matches;
-    // }
 }
 
 public record struct Matches(Dictionary<int, List<int>> IndexMatches, Dictionary<int, List<int>> CandidateMatches);
-
-/*
-    Algorithm:
-    - Candidate is in <= 3 cells (that's an implication of the point above)
-    - Candidate is in two cells, both with two candidates, and they matches (then naked pair)
-    - Candidate is in three cells, all with three candidates, and they match (then naked triple)
-    - Candidate is in three cells, all with two or three candidates, all with just three candidates in total (then naked triple)
-    - Candidate is in two cells, all with two candidates, all with just three candidates in total (then naked triple)
-    - The 3 candidate values can be removed from the rest of the cells in the unit
-
-    Data format:
-    - Dictionary of candidate, List of indexes
-    - If candidate list length is >3, skip
-    - If candidate list is 3, do x
-    - If candidate list is 2, do y
-*/
-
-/*
-            if (TryFindPartialMatchesForCellCandidates(puzzle, cell, line, out Matches matches))
-            {
-                var (candidateMatches, indexMatches) = matches;
-                foreach (int candidate in twoOrThreeCount)
-                {
-                    List<int> indices = matches[candidate];
-
-                    List<int> matches = FindMatches(puzzle, cellCandidates, indices);
-
-                    // Naked pairs
-                    if (candidateCount is 2)
-                    {
-                        if (matches.Count is 1)
-                        {
-                            List<int> alignedIndices = [ cell, matches[0] ];
-                            if (TryFindSolution(puzzle, cellCandidates, line, alignedIndices, out Solution? s))
-                            {
-                                solution = Puzzle.UpdateSolutionWithNextSolution(solution, s);
-                            }
-                        }
-                    }
-                    // candidateCount is 3
-                    // Naked triple of form 3/3/3
-                    else if (matches.Count is 2 && matches.Where(x => matches[x].Count ))
-                    {
-                        List<int> alignedIndices = [ cell, matches[0], matches[1] ];
-                        if (TryFindSolution(puzzle, cellCandidates, line, alignedIndices, out Solution? s))
-                        {
-                            solution = Puzzle.UpdateSolutionWithNextSolution(solution, s);
-                        }
-                    }
-
-                }
-            }
-*/
