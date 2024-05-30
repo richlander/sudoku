4aaa2f4e5d6f9e171bfd278fc6bcd64a2ab3e1b9
Richard Lander <rlander@microsoft.com>
2024-05-29 22:38:58 -0700

    Remove dead code

--- BacktrackerBenchmarks/Program.cs
+++ BacktrackerBenchmarks/Program.cs
@@ -1,25 +1,3 @@
-﻿// using System.Diagnostics;
-
-// string puzzle = "003020600900305001001806400008102900700000008006708200002609500800203009005010300";
-// int[] board = Utils.GetNumberPuzzle(puzzle);
-
-// var watch = Stopwatch.StartNew();
-// if (BacktrackerFour.Backtracker.Solve(board, out int[]? solution))
-// {
-//     watch.Stop();
-//     Console.WriteLine($"Puzzle is valid; Time: {watch.Elapsed.TotalMilliseconds}");
-
-// }
-// else
-// {
-//     Console.WriteLine("Puzzle is invalid");
-// }
-
-// if (solution is not null)
-// {
-//     Utils.PrintBoard(solution);
-// }
-
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 var summary = BenchmarkRunner.Run<BacktrackerBenchmarks>();
